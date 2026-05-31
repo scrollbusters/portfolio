ce5bba757556caba42f74e57d58045479c3950d0
scrollbusters <scrollbusters@gmail.com>
2026-06-01 00:47:41 +0500

    Add files via upload

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -25,8 +25,8 @@ export const Footer = () => {
           <Text onBackground="neutral-weak">© {currentYear} /</Text>
           <Text paddingX="4">{person.name}</Text>
           <Text onBackground="neutral-weak">
-            {/* Usage of this template requires attribution. Please don't remove the link to Once UI unless you have a Pro license. */}
-            / Build your portfolio with{" "}
+            {/* Keeping attribution as required by Once UI CC license */}
+            / Built with{" "}
             <SmartLink href="https://once-ui.com/products/magic-portfolio">Once UI</SmartLink>
           </Text>
         </Text>
